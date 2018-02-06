78b2b13003c6a6cc691c83100e5500ef4427aa81
Erik Forsström <erikforsstrom.kode@gmail.com>
2018-02-06 11:10:54 +0100

    added more files

---
+++ assets/js/todos.js
@@ -0,0 +1,21 @@
+//Check of todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Press X to delete todo
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	e.stopProgagation();
+});
+
+//Listen to new entries in todo list
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li>" + "<span>X</span> " + todoText +"</li>");
+	}
+});
